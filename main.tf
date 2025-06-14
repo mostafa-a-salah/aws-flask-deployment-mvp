8a7558399f2085be983dcc3ce32ec1ba330fed82
mostafa-a-salah <moasalah001@gmail.com>
2025-06-14 21:50:59 +0300

    Add aws_security_group resource and attach to instance

--- main.tf
+++ main.tf
@@ -13,8 +13,33 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-020cba7c55df1f615" # Ubuntu 24.04 LTS in us-east-1
   instance_type = "t2.micro"             # Free Tier eligible
-
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
   tags = {
     Name = "MyWebAppServer-Terraform"
+  }
+}
+resource "aws_security_group" "app_sg" {
+  name        = "webapp-sg"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
